88e45d0e4b49897e3b08cfa0ec728ce62c2c562a
koksalp <alp_koksal@hotmail.com>
2024-12-23 08:17:55 +0300

    create context for authentication

---
+++ src/context/AuthContext.js
@@ -0,0 +1,51 @@
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext({});
+
+export default function AuthProvider({ children }) {
+  const initialUserState = {
+    userId: null, // "64bbd73ea640646fce97ec5b",
+    email: null, // "admin@test.com",
+    role: null, // null,
+    isSignedIn: false, // false,
+  };
+
+  function reducer(userState, action) {
+    switch (action.type) {
+      case "SIGN-IN":
+        return {
+          ...userState,
+          userId: action.userId,
+          email: action.email,
+          role: action.role,
+          isSignedIn: true,
+        };
+      case "SIGN-OUT":
+        return initialUserState;
+      default:
+        return userState;
+    }
+  }
+
+  const [userState, dispatch] = useReducer(reducer, initialUserState);
+
+  function handleUserSignIn(userId, email, role) {
+    dispatch({ type: "SIGN-IN", userId: userId, email: email, role: role });
+  }
+
+  function handleUserSignOut() {
+    dispatch({ type: "SIGN-OUT" });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        userState,
+        handleUserSignIn,
+        handleUserSignOut,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
