ce2d54307b0c9a1d0b5763a7f28fa4771c0a7d0e
koksalp <alp_koksal@hotmail.com>
2024-12-23 11:29:36 +0300

    control app on changes

---
+++ src/components/app-wide/App.jsx
@@ -0,0 +1,52 @@
+import { useContext, useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { getCookie, deleteCookie } from "cookies-next";
+import * as cookies from "@/helpers/constants/cookie_keys.js";
+import { AuthContext } from "@/context/AuthContext";
+import TopMenu from "@/components/app-wide/TopMenu";
+export default function App({ children }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const authCtx = useContext(AuthContext);
+
+  async function handleRouting() {
+    let cookieUser = getCookie(cookies.user);
+    if (cookieUser === undefined) {
+      return router.replace("/login");
+    }
+
+    cookieUser = JSON.parse(cookieUser);
+    if (!cookieUser["is_signed_in"]) {
+      deleteCookie(cookies.user);
+      return router.replace("/login");
+    }
+
+    authCtx.handleUserSignIn(
+      cookieUser["user_id"],
+      cookieUser["email"],
+      cookieUser["role"]
+    );
+  }
+
+  useEffect(() => {
+    if (authCtx.userState.isSignedIn && pathname === "/login") {
+      router.replace("/");
+    }
+    if (!authCtx.userState.isSignedIn) {
+      handleRouting();
+    }
+  }, [authCtx.userState.isSignedIn]);
+
+  if (!authCtx.userState.isSignedIn && pathname !== "/login") {
+    return <></>;
+  }
+
+  return (
+    <>
+      {authCtx.userState.isSignedIn && pathname !== "/login" && <TopMenu />}
+      <div>{children}</div>
+    </>
+  );
+}
