dae170229f15c03ef7ba352cff311497b077a142
koksalp <alp_koksal@hotmail.com>
2024-12-24 11:26:58 +0300

    add endpoint

--- src/helpers/constants/endpoints.js
+++ src/helpers/constants/endpoints.js
@@ -1,4 +1,5 @@
-export const login = "/api/login"; 
+export const login = "/api/login";
 
-export const courses = "/api/courses"; 
-export const grades="/api/grades"; 
+export const courses = "/api/courses";
+export const grades = "/api/grades";
+export const exams = "/api/exams";
