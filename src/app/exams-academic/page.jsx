66ffa1980c652501bd1eb5fc5c747e1ec89fc47c
koksalp <alp_koksal@hotmail.com>
2024-12-24 11:45:53 +0300

    create  pages

---
+++ src/app/exams-academic/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function ExamsAcademicPage() {
+  return <h1>exams academic </h1>;
+}
---
+++ src/app/grades-academic/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function GradesAcademicPage() {
+  return <h1>grades academic </h1>;
+}
