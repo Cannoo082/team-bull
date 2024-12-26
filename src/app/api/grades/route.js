448ae0163cc87d0bab511207a122fe5c051c1e41
koksalp <alp_koksal@hotmail.com>
2024-12-26 16:56:21 +0300

    format code

--- src/app/api/grades/route.js
+++ src/app/api/grades/route.js
@@ -1,13 +1,21 @@
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const userId = searchParams.get("userId");
-  const courseId=searchParams.get("courseId"); 
-  if (userId === null || courseId===null    ) {
-    return Response.json({ message: "Provide a user id and a course id" }, { status: 400 });
+  const courseId = searchParams.get("courseId");
+  if (userId === null || courseId === null) {
+    return Response.json(
+      { message: "Provide a user id and a course id" },
+      { status: 400 }
+    );
   }
-
+  console.log(userId, courseId); 
   const grades = [
-    { type: "quiz", weight: 5, grade: Math.ceil(Math.random()*100), name: "Quiz 1" },
+    {
+      type: "quiz",
+      weight: 5,
+      grade: Math.ceil(Math.random() * 100),
+      name: "Quiz 1",
+    },
     { type: "quiz", weight: 5, grade: 40, name: "Quiz 2" },
     { type: "midterm", weight: 20, grade: 20, name: "Midterm" },
     { type: "quiz", weight: 5, grade: 100, name: "Quiz 3" },
