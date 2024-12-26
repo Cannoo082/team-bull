088034afe05d3568f8aad5dbac23bc38936d3ca2
koksalp <alp_koksal@hotmail.com>
2024-12-26 18:04:34 +0300

    return all exams with the given course id

--- src/app/api/exams/route.js
+++ src/app/api/exams/route.js
@@ -1,22 +1,41 @@
+import { execute } from "@/backend/db";
+
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
-  const userId = searchParams.get("userId");
   const courseId = searchParams.get("courseId");
-  if (userId === null || courseId === null) {
-    return Response.json(
-      { message: "Provide a user id and a course id" },
-      { status: 400 }
-    );
+  if (courseId === null) {
+    return Response.json({ message: "Provide a course id" }, { status: 400 });
   }
 
-  const grades = [
-    { type: "quiz", weight: 5, date: "10-11-2024", name: "Quiz 1" },
-    { type: "quiz", weight: 5, date: "11-11-2024", name: "Quiz 2" },
-    { type: "midterm", weight: 20, date: "12-11-2024", name: "Midterm 1" },
-    { type: "quiz", weight: 5, date: "13-11-2024", name: "Quiz 3" },
-    { type: "quiz", weight: 5, date: "14-11-2024", name: "Quiz 4" },
-    { type: "midterm", weight: 20, date: "15-11-2024", name: "Midterm 2" },
-    { type: "final", weight: 40, date: "18-11-2024", name: "Final" },
-  ];
-  return Response.json(grades);
+  const sql =
+    "SELECT e.* FROM course_schedules cs JOIN exams e ON cs.CRN=e.CRN WHERE cs.CourseID=?";
+  const params = [courseId];
+
+  const exams = await execute(sql, params);
+  if (exams === undefined) {
+    return Response.json({ message: "Failed to load exams" }, { status: 500 });
+  }
+
+  return Response.json(
+    exams.map((exam) => {
+      const date = exam.ExamDate;
+      if (date) {
+        var formattedDate = new Date(date);
+        formattedDate = formattedDate.toLocaleDateString("tr-TR", {
+          timeZone: "Europe/Istanbul",
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        });
+      }
+      return {
+        CRN: exam.CRN,
+        name: exam.ExamName,
+        date: formattedDate,
+        start_time: exam.ExamStartTime,
+        end_time: exam.ExamEndTime,
+        location: exam.ExamLocation,
+      };
+    })
+  );
 }
