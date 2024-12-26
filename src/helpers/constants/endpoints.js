40e60ff46d3aaa09b73081ea05a4a0e137a3454c
koksalp <alp_koksal@hotmail.com>
2024-12-26 18:40:34 +0300

    comment added at top

--- src/helpers/constants/endpoints.js
+++ src/helpers/constants/endpoints.js
@@ -1,7 +1,8 @@
+// endpoints for API routes
 export const login = "/api/login";
 
 export const courses = "/api/courses";
 export const grades = "/api/grades";
 export const exams = "/api/exams";
 
-export const notifications = "/api/notifications"; 
+export const notifications = "/api/notifications";
