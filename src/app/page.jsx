144aab2aa48b042d5b9bdbcabdfdbd6d83bc4a0a
koksalp <alp_koksal@hotmail.com>
2024-12-26 16:56:00 +0300

    get notifications

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,29 @@
 "use client";
 
 import { AuthContext } from "@/context/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { sendRequestGetNotifications } from "@/helpers/functions/http";
 export default function Home() {
   const authCtx = useContext(AuthContext);
+  const [notifs, setNotifs] = useState(null);
+
+  useEffect(() => {
+    async function handleGetNotifications() {
+      const data = await sendRequestGetNotifications(authCtx.userState.userId);
+      if (data === undefined) {
+        return alert(errMsg.default);
+      }
+
+      if (data.error) {
+        return alert(data.message);
+      }
+
+      setNotifs(data);
+    } 
+
+    handleGetNotifications(); 
+  }, []);
   console.log(authCtx); 
+  console.log(notifs); 
   return <h1>Homepage </h1>;
 }
