c578cc8a1f0aa109c84057644dc80fc7c900a630
koksalp <alp_koksal@hotmail.com>
2024-12-26 16:55:17 +0300

    add endpoint

--- src/helpers/constants/endpoints.js
+++ src/helpers/constants/endpoints.js
@@ -3,3 +3,5 @@ export const login = "/api/login";
 export const courses = "/api/courses";
 export const grades = "/api/grades";
 export const exams = "/api/exams";
+
+export const notifications = "/api/notifications"; 
