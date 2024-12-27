c6af7410b93914e7537c3f5c4917c65561e28f1f
koksalp <alp_koksal@hotmail.com>
2024-12-27 15:49:22 +0300

    add name for pages

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import Provider from "@/components/app-wide/Provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "App Name ",
+  title: "Student Administration System",
   description: "Student Administration System",
 };
 
