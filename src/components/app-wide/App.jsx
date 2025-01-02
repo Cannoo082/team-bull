7eb70089187e928ef699d63553871eb20148ecc7
koksalp <alp_koksal@hotmail.com>
2025-01-02 17:25:50 +0300

    change styling

--- src/components/app-wide/App.jsx
+++ src/components/app-wide/App.jsx
@@ -46,7 +46,7 @@ export default function App({ children }) {
   return (
     <>
       {authCtx.userState.isSignedIn && pathname !== "/login" && <TopMenu />}
-      <div style={{ margin: "0 1rem 1rem 1rem" }}>{children}</div>
+      <div style={pathname!=="/settings" ? { margin: "0 1rem 1rem 1rem" } : {}}>{children}</div>
     </>
   );
 }
