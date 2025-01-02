b1f5a524b6eb98fda58682495d9d37d68ba3c1cf
koksalp <alp_koksal@hotmail.com>
2025-01-02 17:27:38 +0300

    create component

---
+++ src/components/pages/SettingsPage/ChangeEmail.jsx
@@ -0,0 +1,7 @@
+import styles from "@/styles/components/ChangeEmail.module.css";
+
+import TextField from '@mui/material/TextField'; 
+
+export default function ChangeEmail() {
+  return <div style={{ color: "white" }}>change email </div>;
+}
