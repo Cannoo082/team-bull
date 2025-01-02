a306e9e8690af888571cf65c2e4c783e7d8c80a6
koksalp <alp_koksal@hotmail.com>
2025-01-02 13:13:14 +0300

    implement the attendance page

--- src/app/attendance-student/page.jsx
+++ src/app/attendance-student/page.jsx
@@ -1,14 +1,17 @@
 "use client";
 
 import styles from "@/styles/pages/AttendanceStudent.module.css";
-
+import { formatString } from "@/helpers/functions/util";
+import Table from "@/components/UI/Table";
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { sendRequestGetAttendance } from "@/helpers/functions/http";
-
+import Dropdown from "@/components/UI/Dropdown";
+const defaultVal = "";
 export default function AttendanceStudent() {
   const authCtx = useContext(AuthContext);
-  const [attendance, setAttendace] = useState(null);
+  const [attendance, setAttendance] = useState(null);
+  const [selected, setSelected] = useState(defaultVal);
   useEffect(() => {
     async function handleAttendance() {
       const data = await sendRequestGetAttendance(authCtx.userState.userId);
@@ -20,10 +23,53 @@ export default function AttendanceStudent() {
         return alert(data.message);
       }
 
-      console.log(data);
+      setAttendance(data);
     }
 
     handleAttendance();
   }, []);
-  return <h1>Attendance </h1>;
+  function handleAttendanceChange(event) {
+    setSelected(event.target.value);
+  }
+
+  if (!attendance) {
+    return null;
+  }
+
+  if (!Object.keys(attendance).length) {
+    return (
+      <>
+        <h1>Attendance</h1>
+        <p>No records found</p>
+      </>
+    );
+  }
+  console.log(attendance);
+  return (
+    <div className={styles.container}>
+      <h1>Attendance</h1>
+      <Dropdown
+        sx={{ maxWidth: 180, marginTop: "1rem", marginBottom: "2rem" }}
+        options={Object.keys(attendance).map((each) => ({
+          name: each,
+        }))}
+        label="Attendance"
+        onChange={handleAttendanceChange}
+        currentValue={selected}
+        optionKey="name"
+        optionValue="name"
+        optionFormattedValue="name"
+      />
+      {selected && (
+        <Table
+          columns={["CourseCode", "Week", "Status"]}
+          rows={attendance[selected]}
+          rowKey="Week"
+          emptyValue="-"
+          handleColumnFormat={(word) => formatString(word, ["_"])}
+          sx={{ width: "40%" }}
+        />
+      )}
+    </div>
+  );
 }
